4ba73d44e5fccffd56d41e4478f0320b8d3d0eb2
Philip Howley <howley.phil@gmail.com>
2015-07-23 17:01:05 -0700

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
-  get 'bikes/index'
+  root 'static_pages#index'
 
-  get 'bikes/new'
-
-  get 'bikes/show'
-
-  get 'bikes/edit'
-
-  get 'index/edit'
-
-  get 'index/show'
-
-  get 'index/new'
+  resources :bikes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
